46e394404edb4f4f24d2407a86b3655259282265
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-05-19 12:09:04 -0300

    Refactor main startup flow

--- payment-request-api/internal/bootstrap/bootstrap.go
+++ payment-request-api/internal/bootstrap/bootstrap.go
@@ -1,9 +1,6 @@
 package bootstrap
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/JoaoVitorML-BR/payment-api-go/payment-request-api/internal/config"
 	handler "github.com/JoaoVitorML-BR/payment-api-go/payment-request-api/internal/payment"
 	"github.com/JoaoVitorML-BR/payment-api-go/payment-request-api/internal/server"
@@ -11,23 +8,10 @@ import (
 )
 
 // NewRouter initializes the payment service and sets up the HTTP router with the appropriate handlers.
-func NewRouter(cfg *config.Config) (*gin.Engine, error) {
-	log.Println("Initializing Payment Router")
+func NewRouter(cfg *config.Config) *gin.Engine {
+	paymentRepository := handler.NewPaymentRepositoryDB(cfg.Pool)
+	paymentService := handler.NewPaymentService(paymentRepository)
+	paymentHandler := handler.NewPaymentHandler(paymentService)
 
-	paymentRepository, err := handler.NewPaymentRepositoryDB(cfg.Pool)
-	if err != nil {
-		return nil, fmt.Errorf("init payment repository: %w", err)
-	}
-
-	paymentService, err := handler.NewPaymentService(paymentRepository)
-	if err != nil {
-		return nil, fmt.Errorf("init payment service: %w", err)
-	}
-
-	paymentHandler, err := handler.NewPaymentHandler(paymentService)
-	if err != nil {
-		return nil, fmt.Errorf("init payment handler: %w", err)
-	}
-
-	return server.SetupRouter(paymentHandler), nil
+	return server.SetupRouter(paymentHandler)
 }
