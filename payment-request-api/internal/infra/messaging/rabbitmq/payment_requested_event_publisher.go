6e62dc6f8cf896c86541ee761ea78c337898d56a
JoaoVitorML-BR <jvmlira33@gmail.com>
2026-05-19 12:11:03 -0300

    Declare and bind the payment requests queue

--- payment-request-api/internal/infra/messaging/rabbitmq/payment_requested_event_publisher.go
+++ payment-request-api/internal/infra/messaging/rabbitmq/payment_requested_event_publisher.go
@@ -1,6 +1,111 @@
 package rabbitmq
 
-// Implemnts in future the RabbitMQPaymentRequestedEventPublisher struct and its Publish method to send events to RabbitMQ.
-// func (p *RabbitMQPaymentRequestedEventPublisher) Publish(event *events.PaymentRequestedEvent) error {
-	
-// }
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/JoaoVitorML-BR/payment-api-go/payment-request-api/internal/payment/events"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitMQPaymentRequestedEventPublisher struct {
+	URI        string
+	Exchange   string
+	Queue      string
+	RoutingKey string
+}
+
+func NewRabbitMQPaymentRequestedEventPublisher(uri, exchange, queue, routingKey string) *RabbitMQPaymentRequestedEventPublisher {
+	return &RabbitMQPaymentRequestedEventPublisher{
+		URI:        uri,
+		Exchange:   exchange,
+		Queue:      queue,
+		RoutingKey: routingKey,
+	}
+}
+
+func (p *RabbitMQPaymentRequestedEventPublisher) Publish(event *events.PaymentRequestedEvent) error {
+	if event == nil {
+		return fmt.Errorf("nil payment requested event")
+	}
+
+	conn, err := amqp.Dial(p.URI)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	payload, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	exchange := p.Exchange
+	if exchange == "" {
+		exchange = "payment.events"
+	}
+
+	routingKey := p.RoutingKey
+	if routingKey == "" {
+		routingKey = event.EventName
+	}
+
+	if err := ch.ExchangeDeclare(
+		exchange,
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		return err
+	}
+
+	queue := p.Queue
+	if queue == "" {
+		queue = "payment_requests"
+	}
+
+	if _, err := ch.QueueDeclare(
+		queue,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		return err
+	}
+
+	if err := ch.QueueBind(
+		queue,
+		routingKey,
+		exchange,
+		false,
+		nil,
+	); err != nil {
+		return err
+	}
+
+	return ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		routingKey,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:  "application/json",
+			Body:         payload,
+			Timestamp:    event.OccurredAt,
+			DeliveryMode: amqp.Persistent,
+		},
+	)
+}
